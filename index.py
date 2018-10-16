0eb5e2c0a3e1260c3a5f84d223946d3d077d1dee
storres8 <stevetorres7562@gmail.com>
2018-10-16 14:47:35 -0400

    built two simple map functions

---
+++ index.py
@@ -0,0 +1,44 @@
+def square(num):
+ return num**2
+
+my_nums = [1,2,3,4,5]
+
+print(
+  list(
+    map(square,my_nums)
+  )
+)
+
+#prints out [1, 4, 9, 16, 25]
+
+# could also write the above function as:
+
+def square(num):
+    return num**2
+
+my_nums = [6,7,8,9]
+
+for item in map(square, my_nums):
+    print(item)
+
+# this way does not print a list however
+# 1
+# 4
+# 9
+# 16
+# 25
+
+def splicer(string):
+  if len(string)%2==0:
+    return "EVEN"
+  else:
+   return string[0]
+
+names=["steve", "kevin", "adrian"]
+
+print(
+  list(map(splicer, names))
+)
+# this functions returns ['s', 'k', 'EVEN']
+# by mapping over the names string and applying the splicer function
+# onto each of the names in the names list
